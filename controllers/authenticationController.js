ef4e3589e94cf62f517b9ff4de101eba1c0b7841
osama-mustafa <semsem6661@gmail.com>
2024-02-07 09:35:49 +0200

    FEATURE: Create AuthenticationController

---
+++ controllers/authenticationController.js
@@ -0,0 +1,30 @@
+const asyncHandler = require("../middlwares/asyncHandler");
+
+
+exports.register = asyncHandler(async (req, res) => {
+
+});
+
+exports.login = asyncHandler(async (req, res) => {
+
+});
+
+exports.getAuthenticatedUser = asyncHandler(async (req, res) => {
+
+});
+
+exports.logout = asyncHandler(async (req, res) => {
+
+});
+
+exports.forgotPassword = asyncHandler(async (req, res) => {
+
+});
+
+exports.resetPassword = asyncHandler(async (req, res) => {
+
+});
+
+exports.resetPassword = asyncHandler(async (req, res) => {
+
+});
