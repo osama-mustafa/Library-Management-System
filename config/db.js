a2225dde646213389aeca18a472576012f47695e
osama-mustafa <semsem6661@gmail.com>
2024-02-26 22:09:47 +0200

    FEATURE: Dockerize The Application

--- config/db.js
+++ config/db.js
@@ -5,13 +5,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASS, {
+//     host: 'mysql',
+//     dialect: 'mysql',
+// });
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
+    'library_system',
+    'root',
+    '', {
+    host: 'mysql',
+    dialect: 'mysql',
 });
 
 const connectDB = async () => {
@@ -19,6 +26,10 @@ const connectDB = async () => {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
     } catch (error) {
+        console.log(process.env.DB_NAME, 'process.env.DB_NAME')
+        console.log(process.env.DB_USER, 'process.env.DB_USER')
+        console.log(process.env.DB_PASS, 'process.env.DB_PASS')
+        console.log(process.env.DB_HOST, 'process.env.DB_HOST')
         console.error('Unable to connect to the database:', error);
     }
 }
