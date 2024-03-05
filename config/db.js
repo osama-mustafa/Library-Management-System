6ea3434dbf90c622190bd753705d39c6d3ef6654
osama-mustafa <semsem6661@gmail.com>
2024-03-05 02:32:09 +0200

    FIX: Fix DB_HOST in docker-compose File

--- config/db.js
+++ config/db.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASS, {
-    host: 'localhost',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
 });
 
