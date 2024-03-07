5628e157e32e741093ffd77a8793133b846b3338
osama-mustafa <semsem6661@gmail.com>
2024-03-07 22:45:54 +0200

    UPDATE: Update README

--- app.js
+++ app.js
@@ -6,17 +6,33 @@ const environment = process.env.NODE_ENV
 const { connectDB } = require('./config/db');
 const mountRoutes = require('./routes/index');
 const rateLimitMiddleware = require('./middlwares/rateLimitMiddleware');
-const helmet = require('helmet')
+const helmet = require('helmet');
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs");
+const YAML = require('yaml');
+
 
 
 // Connect to MySQL DB
 connectDB();
 
+// Parse application/json
 app.use(express.json());
+
+// Enable rate limiting
 app.use(rateLimitMiddleware);
+
+// Use Helmet middleware for security
 app.use(helmet())
 
+// Load Routes
 mountRoutes(app);
+
+// Swagger UI Express for API documentation
+const swaggerFile = fs.readFileSync('./swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(swaggerFile)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.listen(port, () => {
     console.log(`Library Management System listening on ${port} port in ${environment} environment`)
